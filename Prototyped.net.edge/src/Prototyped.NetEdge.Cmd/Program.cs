8ad41a8f9f0da1ab4cda5bd60b9e659bed068c49
JohnnyBeProgramming <Johnathan.Lodder@Gmail.com>
2015-03-08 19:00:50 +0200

    Created new C# project for interop with EdgeJS

---
+++ Prototyped.net.edge/src/Prototyped.NetEdge.Cmd/Program.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using Prototyped.NetEdge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototyped.Cmd.EdgeJS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var col = Console.ForegroundColor;
+            try
+            {
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                Console.WriteLine(" - EdgeJS and Microsoft.net ");
+                Console.WriteLine("-------------------------------------------------------------------------------");
+
+                // Define some test data
+                dynamic data = new
+                {
+                    ident = "C#.NET",
+                    start = DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds,
+                };
+
+                // Create an EdgeJS connection to node
+                using (var edge = AppEdge.Create())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(" - [ C# ] EdgeJS is now active!");
+                    Console.ForegroundColor = col;
+
+                    Console.WriteLine(" - [ C# ] NodeJS <= \r\n" + JsonConvert.SerializeObject(data, Formatting.Indented));
+                    Console.WriteLine("-------------------------------------------------------------------------------");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    
+                    // Call the async task and wait for it to complete                    
+                    var task = edge.Ping(data);
+                    task.Wait();
+
+
+                    Console.ForegroundColor = col;
+                    Console.WriteLine("-------------------------------------------------------------------------------");
+                    Console.WriteLine(" - [ C# ] Result => \r\n" + JsonConvert.SerializeObject(task.Result, Formatting.Indented));
+                }
+
+                Console.WriteLine("-------------------------------------------------------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Red;
+                var i = 0;
+                while (ex != null)
+                {
+                    Console.WriteLine(" - [ " + i + " ] Error: " + ex.Message);
+                    ex = ex.InnerException;
+                    i++;
+                }
+                Console.ForegroundColor = col;
+                Console.WriteLine("-------------------------------------------------------------------------------");
+            }
+            finally
+            {
+                Console.ForegroundColor = col;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prototyped.net.edge/src/Prototyped.NetEdge/AppEdge.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EdgeJs;
+
+namespace Prototyped.NetEdge
+{
+    public class AppEdge : IDisposable
+    {
+        public Exception LastError { get; set; }
+
+        public AppEdge() { }
+        public void Dispose() { }
+
+        public async Task<object> Ping(dynamic payload)
+        {
+            var func = Edge.Func(@"
+                return function (data, callback) {
+                    try
+                    {
+                        console.log(' - [ JS ] NodeJS welcomes \'' + data.ident + '\'!');
+                        var extend = require('node.extend');
+                        callback(null, extend(data, {
+                            ended: Date.now(),
+                            result: true,
+                        }));
+                    } catch (ex) {
+                        data.error = ex;
+                        data.ended = Date.now();
+                        callback(ex, data);
+                    }
+                }
+            ");
+
+            return await func(payload);
+        }
+
+        public static AppEdge Create()
+        {
+            try
+            {
+                // Create a new instance
+                var edge = new AppEdge { };
+
+                // return new instance
+                return edge;
+            }
+            catch (Exception ex)
+            {
+                // Something went wrong
+                throw ex;
+            }
+        }
+
+    }
+}
